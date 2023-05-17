add08d84a1a37d435c240fa68e8673d8d0525b48
Zozo <ling911024@gmail.com>
2023-05-17 15:20:57 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import App_91 from './App_91';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App_91 />
+  </React.StrictMode>
+);
+
+
---
+++ src/pages/WomenProductStartPage_91.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+
+function WomenProductStartPage_91() {
+  return (
+    <div className="shop-page">
+    <div className="collection-page">
+      <h1 className="title">Womens</h1>
+      <div className="items">
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/7CQVJNm/blue-tank.png" />
+          <div className="collection-footer">
+            <span className="name">Blue Tanktop</span>
+            <span className="price">25</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img
+            className="image"
+            src="https://i.ibb.co/4W2DGKm/floral-blouse.png"
+          />
+          <div className="collection-footer">
+            <span className="name">Floral Blouse</span>
+            <span className="price">20</span>
+          </div>
+          <button className="custom-button">Add to Card</button>
+        </div>
+        <div className="collection-item">
+          <img
+            className="image"
+            src="https://i.ibb.co/KV18Ysr/floral-skirt.png"
+          />
+          <div className="collection-footer">
+            <span className="name">Floral Dress</span>
+            <span className="price">80</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img
+            className="image"
+            src="https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png"
+          />
+          <div className="collection-footer">
+            <span className="name">Red Dots Dress</span>
+            <span className="price">80</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img
+            className="image"
+            src="https://i.ibb.co/KmSkMbH/striped-sweater.png"
+          />
+          <div className="collection-footer">
+            <span className="name">Striped Sweater</span>
+            <span className="price">45</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img
+            className="image"
+            src="https://i.ibb.co/v1cvwNf/yellow-track-suit.png"
+          />
+          <div className="collection-footer">
+            <span className="name">Yellow Track Suite</span>
+            <span className="price">135</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+        <div className="collection-item">
+          <img className="image" src="https://i.ibb.co/qBcrsJg/white-vest.png" />
+          <div className="collection-footer">
+            <span className="name">White Blouse</span>
+            <span className="price">20</span>
+          </div>
+          <button className="custom-button">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
